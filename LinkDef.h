92c4f01096d23f2124639576c229de08c6875266
Andreas Mathis <andreas.mathis@cern.ch>
2018-11-07 10:55:41 +0100

    Add new classes to LinkDef

--- LinkDef.h
+++ LinkDef.h
@@ -1,5 +1,7 @@
 #ifdef __CINT__
 #pragma link C++ class FileReader;
 #pragma link C++ class Fitter;
+#pragma link C++ class global;
 #pragma link C++ class Plotter;
+#pragma link C++ class Spectrum;
 #endif
