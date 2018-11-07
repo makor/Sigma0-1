5f616bf67f9083e03a24b2545b0e01fb2c8ff68a
Andreas Mathis <andreas.mathis@cern.ch>
2018-11-07 10:30:24 +0100

    Add global parameter class

---
+++ global.cxx
@@ -0,0 +1,3 @@
+#include "global.h"
+
+std::vector<float> globalpTbins = {{1.1, 1.6, 2.0, 2.4, 2.8, 3.4, 4.0, 5.0, 8.0}};
---
+++ global.h
@@ -0,0 +1,7 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include <vector>
+
+extern std::vector<float> globalpTbins;
+#endif
