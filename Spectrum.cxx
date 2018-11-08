e43584abcb5f8de3e75ed1fd16bc6ad1cbf6a46d
Andreas Mathis <andreas.mathis@cern.ch>
2018-11-08 09:37:12 +0100

    Enhance the Spectrum class
    o add function to rebin histograms according to PWGLF
    o add function to get empty hist with variable bin size
    o add documentation

--- Spectrum.cxx
+++ Spectrum.cxx
@@ -1,5 +1,6 @@
 #include "Spectrum.h"
 #include <iostream>
+#include "global.h"
 
 Spectrum::Spectrum()
     : fRecSpectrum(nullptr),
@@ -10,12 +11,152 @@ Spectrum::Spectrum()
 
 void Spectrum::ComputeCorrectedSpectrum() {
   if (!fRecSpectrum || !fMCSpectrum || !fMCTruth) {
-    std::cerr << "Spectrum missing!\n";
+    std::cerr << "ERROR: Spectrum missing!\n";
     return;
   }
-  fEfficiency = (TH1F*)fMCSpectrum->Clone(Form("%s_Efficiency", fMCSpectrum->GetName()));
+  TString name = fMCSpectrum->GetName();
+  name += "_Efficiency";
+  fEfficiency = (TH1F*)fMCSpectrum->Clone(name);
   fEfficiency->Divide(fMCTruth);
 
-  fCorrSpectrum = (TH1F*)fRecSpectrum->Clone(Form("%s_Corrected", fMCSpectrum->GetName()));
+  name = fMCSpectrum->GetName();
+  name += "_Corrected";
+  fCorrSpectrum = (TH1F*)fRecSpectrum->Clone(name);
   fCorrSpectrum->Divide(fEfficiency);
+}
+
+TH1F* Spectrum::RebinHisto(const TH1F* originalHist,
+                           const bool statisticalUncertainties,
+                           TF1* fit) const {
+  if (!originalHist) {
+    std::cerr << "ERROR: Input histogram missing!\n";
+    return nullptr;
+  }
+
+  // the fit is used to compute the weights when a bin has to be split. In
+  // case it does not exist we just use an exponential
+  bool doFit = true;
+  if (!fit) {
+    fit = new TF1("fit", "[0]*exp([1]*x)", 0., 10.);
+    fit->SetParameters(1., -1.);
+    doFit = false;
+  }
+
+  const int nbins = globalpTbins.size();
+  double bins[nbins];
+  std::copy(globalpTbins.begin(), globalpTbins.end(), bins);
+
+  TString name = originalHist->GetName();
+  name += "_rebinned";
+  TH1F* rebinnedHist = new TH1F(name, originalHist->GetTitle(), nbins, bins);
+
+  // TODO: the histogram for the fit should include the statistical and
+  // systematic uncertainties
+  name = originalHist->GetName();
+  name += "_fit";
+  auto fitHist = (TH1F*)originalHist->Clone(name);
+
+  for (unsigned int l = 1; l <= rebinnedHist->GetNbinsX(); ++l) {
+    const double lowERebin = rebinnedHist->GetXaxis()->GetBinLowEdge(l);
+    const double upERebin = rebinnedHist->GetXaxis()->GetBinLowEdge(l + 1);
+    const double widthRebin = rebinnedHist->GetXaxis()->GetBinWidth(l);
+    const double e = 1.e-5 * widthRebin;
+
+    double newBinContent = 0.;
+    double newBinError = 0.;
+
+    for (unsigned int k = 1; k <= originalHist->GetNbinsX(); ++k) {
+      const double lowEOrig = originalHist->GetXaxis()->GetBinLowEdge(k);
+      const double upEOrig = originalHist->GetXaxis()->GetBinLowEdge(k + 1);
+      const double widthOrig = originalHist->GetXaxis()->GetBinWidth(k);
+
+      double splitBin = 0.f;
+      if (upEOrig <= lowERebin + e || upERebin <= lowEOrig + e) {
+        continue;
+        // bin k originalHist hi completely outside bin l of rebinnedHist
+      } else if (lowERebin <= lowEOrig + e && upEOrig <= upERebin + e) {
+        // bin k of originalHist completely contained within upEOrign l of
+        // rebinnedHist
+        splitBin = 1.;
+      } else if (lowEOrig <= lowERebin + e || upERebin <= upEOrig + e) {
+        // bin k of originalHist is split by the edge(s) of bin l of
+        // rebinnedHist
+        std::cerr << "Info in RebinSpectrum(): splitting originalHist("
+                  << lowEOrig << "," << upEOrig << ") rebinnedHist("
+                  << lowERebin << "," << upERebin << ")\n";
+
+        if (doFit) {
+          if (k == 1 || fitHist->GetBinContent(k - 1) < 1.e-30) {
+            // k is the first non-empty bin of fitHist, fit bin k and the
+            // two following bin
+            fit->SetRange(fitHist->GetBinLowEdge(k),
+                          fitHist->GetBinLowEdge(k + 3));
+          } else if (k == fitHist->GetNbinsX() ||
+                     fitHist->GetBinContent(k + 1) < 1.e-30) {
+            // k is the last non-empty bin of fitHist, fit bin k and the
+            // two preceeding bins
+            fit->SetRange(fitHist->GetBinLowEdge(k - 2),
+                          fitHist->GetBinLowEdge(k + 1));
+          } else {
+            // k is neither the first nor the last non-empty bin of fitHist
+            fit->SetRange(fitHist->GetBinLowEdge(k - 1),
+                          fitHist->GetBinLowEdge(k + 2));
+          }
+
+          fitHist->Fit(fit, "NR");
+        }
+
+        if (lowEOrig <= lowERebin + e && upEOrig <= upERebin + e) {
+          // bin k of originalHist is split by the low edge of bin l of
+          // rebinnedHist
+          splitBin = fit->Integral(lowERebin, upEOrig) /
+                     fit->Integral(lowEOrig, upEOrig);
+        } else if (lowERebin <= lowEOrig + e && upERebin <= upEOrig + e) {
+          // bin k of originalHist is split by the high edge of bin l of
+          // rebinnedHist
+          splitBin = fit->Integral(lowEOrig, upERebin) /
+                     fit->Integral(lowEOrig, upEOrig);
+        } else if (lowEOrig <= lowERebin + e && upERebin <= upEOrig + e) {
+          // bin k of originalHist completely contains bin l of rebinnedHist
+          splitBin = fit->Integral(lowERebin, upERebin) /
+                     fit->Integral(lowEOrig, upEOrig);
+        } else {
+          std::cerr << "Error in RebinSpectrum(): undefined case: originalHist("
+                    << lowEOrig << "," << upEOrig << ") rebinnedHist("
+                    << lowERebin << "," << upERebin << ")\n";
+          continue;
+        }
+      }
+
+      newBinContent += splitBin * originalHist->GetBinContent(k) * widthOrig;
+      // add the content of bin k of originalHist to the total
+      if (statisticalUncertainties) {
+        newBinError +=
+            std::pow(splitBin * originalHist->GetBinError(k) * widthOrig, 2.f);
+        // add (in quadrature) the uncertainties of bin k of originalHist to the
+        // total
+      } else {
+        newBinError += splitBin * originalHist->GetBinError(k) * widthOrig;
+        // add (linearly) the uncertainties of upEOrign k of originalHist to the
+        // total (for systematic uncertainties)
+      }
+    }
+
+    newBinContent /= widthRebin;
+    if (statisticalUncertainties) newBinError = std::sqrt(newBinError);
+    newBinError /= widthRebin;
+
+    // fill the output histograms
+    rebinnedHist->SetBinContent(l, newBinContent);
+    rebinnedHist->SetBinError(l, newBinError);
+  }
+  return rebinnedHist;
+}
+
+TH1F* Spectrum::GetBinnedHistogram(TString name, TString title) const {
+  const int nbins = globalpTbins.size();
+  double bins[nbins];
+  std::copy(globalpTbins.begin(), globalpTbins.end(), bins);
+  TH1F* newBinnedHist = new TH1F(name, title, nbins, bins);
+  return newBinnedHist;
 }
--- Spectrum.h
+++ Spectrum.h
@@ -1,27 +1,75 @@
 #ifndef Spectrum_H
 #define Spectrum_H
 
+/// \file Spectrum.h
+/// \brief Definition of Spectrum computation class
+/// \author A. Mathis
+
+#include "TF1.h"
 #include "TH1.h"
 
+/// \class Spectrum
+/// This class is used to compute efficiency corrected spectra according to
+/// reconstructed spectra, MC spectra and the MC truth
+/// Furthermore, some nice helper functions are implemented.
 class Spectrum {
  public:
+  /// Constructor
   Spectrum();
+
+  /// Destructor
   ~Spectrum() = default;
 
+  /// Set the reconstructed pT spectrum
+  /// \param spec Reconstructed pT spectrum
   void SetRecSpectrum(TH1F* spec) { fRecSpectrum = spec; }
+
+  /// Set the pT spectrum from MC
+  /// \param spec pT spectrum from MC
   void SetMCSpectrum(TH1F* spec) { fMCSpectrum = spec; }
+
+  /// Set the pT spectrum of the MC truth
+  /// \param spec pT spectrum of the MC truth
   void SetMCTruth(TH1F* spec) { fMCTruth = spec; }
+
+  /// Compute the efficiency-corrected pT spectrum
   void ComputeCorrectedSpectrum();
 
+  /// Get the efficiency vs. pT
+  /// \return Efficiency (fMCSpectrum/fMCTruth)
   TH1F* GetEfficiency() const { return fEfficiency; }
+
+  /// Get the efficiency-corrected pT spectrum
+  /// \return Efficiency-corrected pT spectrum
   TH1F* GetCorrectedSpectrum() const { return fCorrSpectrum; }
 
+  /// Function to rebin histograms taking into account the uncertainties and
+  /// including cases with incompatible bins. Implementation by A. Knospe in
+  /// AliPhysics/PWGLF/RESONANCES/macros/utils/RebinSpectrum.C
+  /// \param originalHist Histogram to be rebinned
+  /// \param statisticalUncertainties Flag whether the histogram contains
+  /// statistical or systematic uncertatinties
+  /// \param fit Fit function to be used for the interpolation in case of
+  /// incompatible bins. If not set, an exponential function is used
+  /// \return Rebinned originalHist
+  TH1F* RebinHisto(const TH1F* originalHist,
+                   const bool statisticalUncertainties = true,
+                   TF1* fit = nullptr) const;
+
+  /// Function to get a histogram with the proper binning according to
+  /// globalpTbins
+  /// \param name Name of the histogram
+  /// \param title Title of the histogram (title, x-axis, yaxis)
+  /// \return Empty histogram with the pT bins specified in globalpTbins
+  TH1F* GetBinnedHistogram(
+      TString name, TString title = "; #it{p}_{T} (GeV/#it{c}; Entries") const;
+
  private:
-  TH1F* fRecSpectrum;
-  TH1F* fMCSpectrum;
-  TH1F* fMCTruth;
-  TH1F* fCorrSpectrum;
-  TH1F* fEfficiency;
+  TH1F* fRecSpectrum;   ///< Reconstructed pT spectrum
+  TH1F* fMCSpectrum;    ///< pT spectrum from MC
+  TH1F* fMCTruth;       ///< pT spectrum of the MC truth
+  TH1F* fCorrSpectrum;  ///< Efficiency-corrected pT spectrum
+  TH1F* fEfficiency;    ///< Efficiency (fMCSpectrum/fMCTruth)
 };
 
 #endif  // Spectrum_H
